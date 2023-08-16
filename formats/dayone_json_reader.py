bffa81850512d0be588dcf54e748f3b4e12d1332
Daniel Duma <danielduma@gmail.com>
2023-08-16 08:03:59 +0300

    fixes

--- formats/dayone_json_reader.py
+++ formats/dayone_json_reader.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 
 from formats.base_reader import BaseReader
 
+FIELDS_TO_IGNORE = ['richText']
 
 class DayOneJSONReader(BaseReader):
     """
@@ -23,12 +24,15 @@ def convert_camel_case_to_snake_case(name):
 def load_dayone_json(zip_file_path):
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         for filename in zip_ref.namelist():
-            if filename.endswith('.json'):  # Only process files with .json extension
+            if filename.endswith('.json'):
                 with zip_ref.open(filename) as file:
                     data = json.load(file)
                     for entry in data['entries']:
                         new_entry = OrderedDict()
                         new_entry['date'] = entry['creationDate']
+
+                        if not entry.get('text'):
+                            continue
 
                         text_lines = entry['text'].split('\n')
                         new_entry['title'] = text_lines[0].lstrip('# ')
@@ -36,7 +40,7 @@ def load_dayone_json(zip_file_path):
 
                         metadata = {}
                         for key, value in entry.items():
-                            if key not in ['creationDate', 'text']:
+                            if key not in ['creationDate', 'text'] and key not in FIELDS_TO_IGNORE:
                                 metadata[convert_camel_case_to_snake_case(key)] = value
 
                         new_entry['metadata'] = metadata
@@ -47,7 +51,7 @@ def load_dayone_json(zip_file_path):
 def main():
     # Replace 'file.xml' with the name of your XML file
     reader = DayOneJSONReader()
-    for data in reader.read_directory('../data', file_mask='*.zip'):
+    for data in reader.read_directory('../data', file_mask='dayone.zip'):
         print(json.dumps(data, indent=4))
 
 
